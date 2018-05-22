9429511cc31dfb2e44b43ed7e0540b47cf6c81ab
SamirJoshi <samirjoshi2018@u.northwestern.edu>
2018-05-21 22:38:08 -0500

    more trig functions and Travis CI setup

--- src/math/trig.rs
+++ src/math/trig.rs
@@ -2,7 +2,7 @@ use ndarray::*;
 use std;
 use num_traits;
 
-/// Computes element-wise sin on an ndarray Array
+/// Computes element-wise sine on an ndarray Array
 pub fn sin<A, D>(arr: &Array<A, D>) -> Array<A, D>
     where D: Dimension,
       A: std::fmt::Debug + std::marker::Copy + num_traits::real::Real,
@@ -12,7 +12,7 @@ pub fn sin<A, D>(arr: &Array<A, D>) -> Array<A, D>
     sin_arr.into_shape(arr.raw_dim()).unwrap()
 }
 
-/// Computes element-wise cos on an ndarray Array
+/// Computes element-wise cosine on an ndarray Array
 pub fn cos<A, D>(arr: &Array<A, D>) -> Array<A, D>
     where D: Dimension,
       A: std::fmt::Debug + std::marker::Copy + num_traits::real::Real,
@@ -22,12 +22,54 @@ pub fn cos<A, D>(arr: &Array<A, D>) -> Array<A, D>
     res_arr.into_shape(arr.raw_dim()).unwrap()
 }
 
+/// Computes element-wise tangent on an ndarray Array
+pub fn tan<A, D>(arr: &Array<A, D>) -> Array<A, D>
+    where D: Dimension,
+      A: std::fmt::Debug + std::marker::Copy + num_traits::real::Real,
+{
+    //TODO: change to actually handle the error
+    let res_arr = Array::from_iter(arr.iter().map(|x| {
+        x.tan()
+    }));
+    res_arr.into_shape(arr.raw_dim()).unwrap()
+}
+
+/// Computes element-wise arc sine on an ndarray Array
+pub fn asin<A, D>(arr: &Array<A, D>) -> Array<A, D>
+    where D: Dimension,
+      A: std::fmt::Debug + std::marker::Copy + num_traits::real::Real,
+{
+    //TODO: change to actually handle the error
+    let res_arr = Array::from_iter(arr.iter().map(|x| x.asin()));
+    res_arr.into_shape(arr.raw_dim()).unwrap()
+}
+
+/// Computes element-wise arc cosine on an ndarray Array
+pub fn acos<A, D>(arr: &Array<A, D>) -> Array<A, D>
+    where D: Dimension,
+      A: std::fmt::Debug + std::marker::Copy + num_traits::real::Real,
+{
+    //TODO: change to actually handle the error
+    let res_arr = Array::from_iter(arr.iter().map(|x| x.acos()));
+    res_arr.into_shape(arr.raw_dim()).unwrap()
+}
+
+/// Computes element-wise arc tangent on an ndarray Array
+pub fn atan<A, D>(arr: &Array<A, D>) -> Array<A, D>
+    where D: Dimension,
+      A: std::fmt::Debug + std::marker::Copy + num_traits::real::Real,
+{
+    //TODO: change to actually handle the error
+    let res_arr = Array::from_iter(arr.iter().map(|x| x.atan()));
+    res_arr.into_shape(arr.raw_dim()).unwrap()
+}
+
 #[cfg(test)]
 mod trig_tests {
     use ndarray::*;
     use num_traits;
     use std;
-    use super::{sin, cos};
+    use super::{sin, cos, tan};
 
     #[test]
     fn sin_tests() {
@@ -47,6 +89,15 @@ mod trig_tests {
         assert!(compare_arrays(&expected_arr, &res_arr));
     }
 
+    //#[test]
+    //fn tan_tests() {
+        //let pi = std::f64::consts::PI;
+        //let input_arr = array![0.0, pi / 4.0, pi / 2.0, pi];
+        //let expected_arr = array![0.0, 1.0, std::f64::INFINITY, 0.0];
+        //let res_arr = tan(&input_arr);
+        //assert!(compare_arrays(&expected_arr, &res_arr));
+    //}
+
     fn compare_arrays<D>(expected_arr: &Array<f64, D>, res_arr: &Array<f64, D>) -> bool
         where D: Dimension,
     {
@@ -55,7 +106,8 @@ mod trig_tests {
 
         while let Some(r) = res_iter.next() {
             let exp = expected_iter.next().unwrap();
-            if (*r - *exp).abs() > 0.0001 {
+            println!("Expected: {}, Res: {}", *exp, *r);
+            if (*r - *exp).abs() > 1e-10 {
                 return false;
             }
         }
