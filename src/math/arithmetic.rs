5657b5d65e90f999f61d45f810ddd3dfa6442744
yannick-mamudo <yannick.mamudo@gmail.com>
2018-05-29 00:42:41 -0500

    Finally figured out how to use macro_rules! :tired_face:

--- src/math/arithmetic.rs
+++ src/math/arithmetic.rs
@@ -5,39 +5,39 @@
 
 use ndarray::*;
 use num_traits;
-use std;
+use std::{fmt::Debug, marker::Copy, ops::Mul};
 
 /// add
 /// takes two ndarrays and adds them
-#[allow(unused)]
-pub fn add<A, D>(arr1: &Array<A, D>, arr2: &Array<A, D>) -> Array<A, D>
-where
-    D: Dimension,
-    A: std::fmt::Debug + std::marker::Copy + num_traits::real::Real,
-{
-    unimplemented!()
-}
 
 /// reciprocal
 /// Return the reciprocal of the argument, element-wise.
 /// Calculates 1/x.
-#[allow(unused)]
-pub fn reciprocal<A, D>(arr: &Array<A, D>) -> Array<A, D>
+pub trait Reciprocal<T, D>
 where
     D: Dimension,
-    A: std::fmt::Debug + std::marker::Copy + std::ops::Div<A>,
-    f64: std::ops::Div<A>,
 {
-    // arr.mapv(|x| 1.0 / x)
-    unimplemented!()
+    fn reciprocal(&self) -> Array<T, D>;
 }
+
+macro_rules! impl_Reciprocal {
+    (for $($t:ty),+) => {
+        $(impl<D: Dimension> Reciprocal<$t, D> for Array<$t, D> {
+            fn reciprocal(&self) -> Array<$t, D> {
+                self.mapv(|x| 1 as $t / x)
+            }
+        })*
+    };
+}
+
+impl_Reciprocal!{for usize, u32, u64, i32, i64, f32, f64}
 
 /// positive
 /// Numerical positive, element-wise.
 pub fn positive<A, D>(arr: &Array<A, D>) -> Array<A, D>
 where
     D: Dimension,
-    A: std::fmt::Debug + std::marker::Copy + num_traits::sign::Signed,
+    A: Debug + Copy + num_traits::sign::Signed,
 {
     arr.mapv(|x| x.abs())
 }
@@ -47,7 +47,7 @@ where
 pub fn negative<A, D>(arr: &Array<A, D>) -> Array<A, D>
 where
     D: Dimension,
-    A: std::fmt::Debug + std::marker::Copy + num_traits::sign::Signed + std::ops::Mul<A>,
+    A: Debug + Copy + num_traits::sign::Signed + Mul,
 {
     arr.mapv(|x| x.abs().neg())
 }
@@ -60,18 +60,13 @@ where
 
 /// power
 /// First array elements raised to powers from second array, element-wise.
-#[allow(unused)]
-pub fn power<A, D>(arr: &Array<A, D>, power: usize) -> Array<A, D>
-where
-    D: Dimension,
-    A: std::fmt::Debug + std::marker::Copy + num_traits::pow::Pow<usize>,
-{
-    // TODO
-    // figure out how to get a type from Self::Output
+// fn power<A, D>(arr: &Array<A, D>, exp: usize) -> Array<A, D>
+// where
+//     D: Dimension,
+//     A: Debug + Copy,
+// {
 
-    // arr.mapv(|x| x.pow(power))
-    unimplemented!()
-}
+// }
 
 /// subtract
 /// Subtract arguments, element-wise.
@@ -94,6 +89,31 @@ where
 
 /// remainder
 /// Return element-wise remainder of division.
+pub trait Remainder<T, D>
+where
+    D: Dimension,
+{
+    fn remainder(&self, arr2: &Array<T, D>) -> Array<T, D>;
+}
+
+macro_rules! impl_Remainder {
+    (for $($t:ty),+) => {
+        $(impl<D: Dimension> Remainder<$t, D> for Array<$t, D> {
+            fn remainder(&self, arr2: &Array<$t, D>) -> Array<$t, D> {
+                let mut res = Array::from_elem(self.dim(), 0 as $t);
+                Zip::from(&mut res)
+                    .and(self)
+                    .and(arr2)
+                    .apply(|x, &y, &z| {
+                        *x = &y % &z;
+                    });
+                res
+            }
+        })*
+    };
+}
+
+impl_Remainder!{for usize, u32, u64, i32, i64, f32, f64}
 
 /// divmod
 /// Return element-wise quotient and remainder simultaneously.
@@ -101,10 +121,7 @@ where
 
 #[cfg(test)]
 mod arithmetic_tests {
-    use super::{negative, positive};
-    use ndarray::*;
-    use num_traits;
-    use std;
+    use super::{negative, positive, Reciprocal, Remainder};
 
     #[test]
     fn positive_test() {
@@ -119,10 +136,29 @@ mod arithmetic_tests {
     #[test]
     fn negative_test() {
         let input_arr = array![1.0, 0.0, -1.0];
-        let expected_arr = array![-1.0, -0.0, -1.0];
+        let expected_arr = array![-1.0, 0.0, -1.0];
         assert_eq!(negative(&input_arr), expected_arr);
 
         // TODO: test limits
         // TODO: test other num types
+    }
+
+    #[test]
+    fn reciprocal_test() {
+        let input_arr = array![1.0, 2.0, 4.0];
+        let expected_arr = array![1.0, 0.5, 0.25];
+        assert_eq!(input_arr.reciprocal(), expected_arr);
+
+        let input_arr_2 = array![1, 2, 4];
+        let expected_arr_2 = array![1, 0, 0];
+        assert_eq!(input_arr_2.reciprocal(), expected_arr_2);
+    }
+
+    #[test]
+    fn remainder_test() {
+        let arr1 = array![10.0, 11.0, 12.0];
+        let arr2 = array![3.0, 4.0, 5.0];
+        let arr3 = array![1.0, 3.0, 2.0];
+        assert_eq!(arr1.remainder(&arr2), arr3);
     }
 }
